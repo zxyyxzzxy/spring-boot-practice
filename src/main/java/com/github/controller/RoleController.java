bd36afb4a33a2b70a20d63a26c76670c3af02bc4
Ethan <154518484@qq.com>
2017-11-01 12:23:50 +0800

    update...

--- src/main/java/com/github/controller/RoleController.java
+++ src/main/java/com/github/controller/RoleController.java
@@ -9,6 +9,7 @@ import com.github.util.DataTableJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +34,13 @@ public class RoleController {
 		DataTableJson<Role> dataTableJson = new DataTableJson<>(roleList);
 
 		return dataTableJson;
+	}
+
+	@ResponseBody
+	@GetMapping("{id}/delete")
+	public Object delete(@PathVariable Integer id) {
+		roleMapper.delete(id);
+		return "";
 	}
 
 }
